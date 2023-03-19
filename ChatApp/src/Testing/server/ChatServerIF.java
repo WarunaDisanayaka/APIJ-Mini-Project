4ff4de5074ca0cc2539131b87696a26b613959a4
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-03-20 01:17:23 +0530

    Client Server - T

---
+++ ChatApp/src/Testing/server/ChatServerIF.java
@@ -0,0 +1,6 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface ChatServerIF extends Remote{
+        void registerChatClient(ChatClientIF chatClient)throws RemoteException;
+        void broadcastMessage(String message)throws RemoteException;
+}
