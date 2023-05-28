bf8599916b5e36d4810068b669b04e2e58755b33
warunahello@gmail.com <102423170+JamilaHasangi@users.noreply.github.com>
2023-05-29 03:01:08 +0530

    Login form updated

--- RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
+++ RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
@@ -1,9 +1,28 @@
 package com.Main.Interface;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Login {
+public class Login extends JFrame{
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton loginButton;
+    private JPanel loginPanel;
+
+
+    public Login(){
+        setContentPane(loginPanel);
+        setTitle("Login");
+        setSize(450,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Login login=new Login();
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - login.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - login.getHeight()) / 2);
+        login.setLocation(x, y);
+    }
 }
