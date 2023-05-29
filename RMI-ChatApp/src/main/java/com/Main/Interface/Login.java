a58148995d690fd6166899907d10fff377d525d2
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-29 08:40:03 +0530

    Added chat group

--- RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
+++ RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
@@ -1,14 +1,22 @@
 package com.Main.Interface;
 
+import com.Main.ChatServer.User;
+import com.Main.database.Database;
+import entity.UserEntity;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class Login extends JFrame{
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField uname;
+    private JPasswordField pwd;
     private JButton loginButton;
     private JPanel loginPanel;
 
+    public Database database=new Database();
 
     public Login(){
         setContentPane(loginPanel);
@@ -16,6 +24,26 @@ public class Login extends JFrame{
         setSize(450,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username=uname.getText().trim();
+                String password=pwd.getText().trim();
+
+                UserEntity userEntity=database.login(username);
+
+                if (userEntity !=null){
+                    String role=String.valueOf(userEntity.getRoleId());
+                    if (Objects.equals(userEntity.getPassword(),password)){
+                        User.setUser(userEntity);
+                        if (Objects.equals(role,"1")){
+
+                        }
+                    }
+                }
+
+            }
+        });
     }
 
     public static void main(String[] args) {
