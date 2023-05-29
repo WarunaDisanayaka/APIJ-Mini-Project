5a5934799fe963643c454322c24126baaf680a04
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-29 09:53:08 +0530

    Update user panel

--- RMI-ChatApp/src/main/java/com/Main/Interface/UserPanel.java
+++ RMI-ChatApp/src/main/java/com/Main/Interface/UserPanel.java
@@ -1,9 +1,24 @@
 package com.Main.Interface;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class UserPanel {
+public class UserPanel extends JFrame {
     private JButton subscribeButton;
     private JButton unsubscribeButton;
-    private JTable table1;
+    private JTable groups;
+    private JPanel grouppanel;
+
+    private DefaultTableModel defaultTableModel;
+
+
+    public UserPanel(){
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(grouppanel);
+        this.pack();
+
+
+    }
+
+
 }
