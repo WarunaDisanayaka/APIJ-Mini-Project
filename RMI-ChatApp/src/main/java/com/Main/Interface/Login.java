11afe3aedbd55c3fd0a459c23b8b5c9b468ba032
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-29 08:03:31 +0530

    Update login function

--- RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
+++ RMI-ChatApp/src/main/java/com/Main/Interface/Login.java
@@ -1,14 +1,21 @@
 package com.Main.Interface;
 
+import com.Main.ChatServer.User;
+import com.Main.database.Database;
+import entity.UserEntity;
+
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class Login extends JFrame{
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField username;
+    private JPasswordField password;
     private JButton loginButton;
     private JPanel loginPanel;
 
+    public Database database=new Database();
 
     public Login(){
         setContentPane(loginPanel);
@@ -16,13 +23,27 @@ public class Login extends JFrame{
         setSize(450,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String uname=username.getText().trim();
+                String pwd=password.getText().trim();
+
+                UserEntity userEntity=new UserEntity();
+
+                if (userEntity!=null){
+                    String role=String.valueOf(userEntity.getRoleId());
+                    if (Objects.equals(userEntity.getPassword(),pwd));
+                    User.setUser(userEntity);
+                    if (Objects.equals(role,1)){
+
+                    }
+
+                }
+
+            }
+        });
     }
 
-    public static void main(String[] args) {
-        Login login=new Login();
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - login.getWidth()) / 2);
-        int y = (int) ((dimension.getHeight() - login.getHeight()) / 2);
-        login.setLocation(x, y);
-    }
+
 }
