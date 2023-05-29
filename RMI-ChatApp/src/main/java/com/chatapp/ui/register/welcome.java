b61d0a55a47bd2137d0378865bac8ea77b7fc7ca
Lasith Randima <74246027+LasithRandima@users.noreply.github.com>
2023-05-29 05:40:15 +0530

    update register

--- RMI-ChatApp/src/main/java/com/chatapp/ui/register/welcome.java
+++ RMI-ChatApp/src/main/java/com/chatapp/ui/register/welcome.java
@@ -12,24 +12,34 @@ public class welcome extends JFrame{
     private JPanel Register;
     private JButton registerButton;
     private JButton loginButton;
+    private JLabel logoaaa;
 
     public welcome() {
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                welcome.super.dispose();
                 JFrame frame1 = new register();
                 frame1.setVisible(true);
-                frame1.setSize(400, 500);
+                frame1.setSize(400, 600);
+
+                // dispose the current frame
+                JButton button = (JButton) e.getSource();
+                JFrame welcome = (JFrame) SwingUtilities.getWindowAncestor(button);
+                welcome.dispose();
+
             }
         });
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                welcome.super.dispose();
                 JFrame frame2 = new login();
                 frame2.setVisible(true);
-                frame2.setSize(400, 500);
+                frame2.setSize(400, 600);
+
+                // dispose the current frame
+                JButton button = (JButton) e.getSource();
+                JFrame welcome = (JFrame) SwingUtilities.getWindowAncestor(button);
+                welcome.dispose();
             }
         });
     }
@@ -39,7 +49,7 @@ public class welcome extends JFrame{
         welcome register = new welcome();
         frame.setContentPane(register.Register);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 500); // Set the desired size here (width, height)
+        frame.setSize(400, 600); // Set the desired size here (width, height)
         frame.setVisible(true);
     }
 
