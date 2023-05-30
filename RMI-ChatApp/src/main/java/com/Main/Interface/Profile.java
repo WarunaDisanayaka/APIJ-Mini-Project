ed80c0b484873c7d8da2b6283e3092b4f952f4e7
Lasith Randima <74246027+LasithRandima@users.noreply.github.com>
2023-05-30 12:52:14 +0530

    bug fix

--- RMI-ChatApp/src/main/java/com/Main/Interface/Profile.java
+++ RMI-ChatApp/src/main/java/com/Main/Interface/Profile.java
@@ -2,6 +2,8 @@ package com.Main.Interface;
 
 import com.Main.ChatServer.User;
 import com.Main.database.Database;
+import entity.UserEntity;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +14,11 @@ public class Profile extends JFrame{
     private JTextField textEmail;
     private JPanel uprofile;
     private JButton button1;
+    private JButton updateProfileButton;
+    private JTextField password;
+    private JTextField textNick;
+
+    public Database database=new Database();
 
     public Profile(){
         setContentPane(uprofile);
@@ -21,8 +28,11 @@ public class Profile extends JFrame{
         setVisible(true);
 
         textID.setText(String.valueOf(User.getId()));
+        textID.setEditable(false);
         textName.setText(User.getUsername());
         textEmail.setText(User.getEmail());
+        password.setText(User.getPassword());
+        textNick.setText(User.getNickname());
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -30,6 +40,26 @@ public class Profile extends JFrame{
                 frame.setVisible(true);
                 frame.setSize(400, 600);
                 Profile.super.dispose();
+            }
+        });
+        updateProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = textName.getText().trim();
+                String pwd = password.getText().trim();
+                String useremail = textEmail.getText().trim();
+                String nickname = textNick.getText().trim();
+
+                UserEntity userEntity = new UserEntity();
+                userEntity.setNickname(nickname);
+                userEntity.setUsername(username);
+                userEntity.setPassword(pwd);
+                userEntity.setEmail(useremail);
+                userEntity.setIsDeleted(0);
+                userEntity.setRoleId(2);
+                
+
+
             }
         });
     }
